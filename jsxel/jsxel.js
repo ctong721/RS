0da2e2635e69c99da155a99ccdf832ad2e273a66
ctong721 <ctong721@163.com>
2020-09-22 18:40:15 +0800

    modified:   jsxel.js 	modified:   main.js

--- jsxel/jsxel.js
+++ jsxel/jsxel.js
@@ -3,12 +3,18 @@ jsxel = {
 			"#AB5236","#5F574F","#C2C3C7","#FFF1E8",
 			"#FF004D","#FFA300","#FFEC27","#00E436",
 			"#29ADFF","#83769C","#FF77AB","#FFCCAA"],
-	scale: 5,
-	
-	init: function(w,h){
+	size: 5,
+
+	random: function(start,end){
+		var temp = start - end + 1;
+		return Math.abs(Math.floor(Math.random()*temp)) + start;
+},
+
+	init: function(w,h,s){
+		this.size = s;
 		c = document.createElement("canvas");
-		c.width = w;
-		c.height = h;
+		c.width = w*this.size;
+		c.height = h*this.size;
 		ctx = c.getContext("2d");
 		document.body.appendChild(c);
 	},
@@ -21,7 +27,7 @@ jsxel = {
 	
 	frect: function(x,y,w,h,col){
 		ctx.fillStyle = col;
-		ctx.fillRect(x,y,w,h);
+		ctx.fillRect(x*this.size,y*this.size,w*this.size,h*this.size);
 	},
 	
 	text: function(x,y,t,col,f="20px Verdana"){
--- jsxel/main.js
+++ jsxel/main.js
@@ -1,6 +1,8 @@
 window.onload = function(){
-	jsxel.init(600,400);
-	jsxel.rect(1,1,200,100,jsxel.color[8],1);
-	jsxel.frect(1,200,200,100,jsxel.color[8]);
-	jsxel.text(30,30,"hello world",jsxel.color[1],"20px Verdana")
-}
+	//alert(jsxel.random(1,5));
+	jsxel.init(100,100,5);
+	for(i=1;i<10;i++){
+		for(j=1;j<10;j++){
+		jsxel.frect(i,j,5,5,jsxel.color[jsxel.random(0,15)]);
+	}}
+};
