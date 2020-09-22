2091b2d87dd1e94a657093240d5dd0ae4abe3d4a
ctong721 <ctong721@163.com>
2020-09-22 19:03:01 +0800

    modified:   main.js

--- jsxel/main.js
+++ jsxel/main.js
@@ -1,8 +1,8 @@
 window.onload = function(){
-	//alert(jsxel.random(1,5));
 	jsxel.init(100,100,5);
 	for(i=1;i<10;i++){
 		for(j=1;j<10;j++){
-		jsxel.frect(i,j,5,5,jsxel.color[jsxel.random(0,15)]);
-	}}
+			jsxel.frect(i,j,5,5,jsxel.color[jsxel.random(0,15)]);
+		}
+	}
 };
